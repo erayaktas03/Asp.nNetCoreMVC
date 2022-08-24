0f8df2669d557c26a3fb405128908f574c9325f5
eray aktas <ERAY AKTA@DESKTOP-4FG3BN5>
2022-08-24 21:14:54 +0300

    Add project files.

---
+++ Asp.nNetCoreMVC/Controllers/MVC01RazorSyntaxController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.nNetCoreMVC.Controllers
+{
+    public class MVC01RazorSyntaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.nNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.nNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
